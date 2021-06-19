ac7f00e16fd44ef2383ee671f9dd6f132985e797
matheen <mohammed_matheen@intuit.com>
2021-06-19 21:03:24 +0530

    first cut

--- app.js
+++ app.js
@@ -1,26 +1,10 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const app = express();
 
-const hostname = '127.0.0.1';
-const port = process.env.PORT || 3000;
-
-const server = http.createServer((req, res) => {
-  if(req.url == '/'){
-    res.end('Home');
-  } else if(req.url == '/about'){
-    res.end('About');
-  } else if (req.url == '/jsonData'){
-      fs.readFile(`${__dirname}/apiData.json`, 'utf-8', (err, data) => {
-      res.end(data);
-    })
-  } else{
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
-  }
-  
+app.get('/', (req, res) => {
+  res.send("Hellooooo")
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(3000, () => {
+  console.log("Running on 3000")
 });
